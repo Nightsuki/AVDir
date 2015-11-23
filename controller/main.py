c3a3f6e2c23e9040cad15073f8287c324aeb5da0
Nightsuki <i@ymr.me>
2015-11-23 11:07:26 +0800

    改一波模板

--- controller/main.py
+++ controller/main.py
@@ -6,6 +6,7 @@ from database import *
 from util.function import humantime
 from itertools import groupby
 
+
 class MainHandler(BaseHandler):
     def initialize(self):
         BaseHandler.initialize(self)
@@ -15,7 +16,8 @@ class MainHandler(BaseHandler):
     def get(self, *args, **kwargs):
         count = Archive.select().count()
         maxpage = count / 6 + 1
-        archives = Archive.select().where((Archive.status == 1) & (Archive.type == "archive")).order_by(Archive.created_time.desc()).offset(0).limit(6)
+        archives = Archive.select().where((Archive.status == 1) & (Archive.type == "archive")).order_by(
+            Archive.created_time.desc()).offset(0).limit(6)
         self.render("list.html", archives=archives, maxpage=maxpage, page=1)
 
 
@@ -30,7 +32,8 @@ class PageHandler(BaseHandler):
         offset = (page - 1) * 6
         count = Archive.select().count()
         maxpage = count / 6 + 1
-        archives = Archive.select().where((Archive.status == 1) & (Archive.type == "archive")).order_by(Archive.created_time.desc()).offset(offset).limit(6)
+        archives = Archive.select().where((Archive.status == 1) & (Archive.type == "archive")).order_by(
+            Archive.created_time.desc()).offset(offset).limit(6)
         self.render("list.html", archives=archives, maxpage=maxpage, page=page)
 
 
@@ -42,10 +45,12 @@ class TagHandler(BaseHandler):
     @gen.coroutine
     def get(self, *args, **kwargs):
         tag = args[0]
-        archives = Archive.select().where((Archive.tags.contains(tag)) & (Archive.status == 1) & (Archive.type == "archive")).order_by(Archive.published_time.desc())
+        archives = Archive.select().where(
+            (Archive.tags.contains(tag)) & (Archive.status == 1) & (Archive.type == "archive")).order_by(
+            Archive.published_time.desc())
         archives_groups = [{'year': year, 'archives': list(archives)}
                            for year, archives in groupby(archives, key=lambda a: humantime(a.published_time, "%Y"))]
-        self.render("timeline.html", archives_groups=archives_groups, first_title=tag)
+        self.render("timeline.html", archives_groups=archives_groups, first_title="Tag: %s" % tag)
 
 
 class FeedHandler(BaseHandler):
@@ -55,6 +60,7 @@ class FeedHandler(BaseHandler):
     @tornado.web.asynchronous
     @gen.coroutine
     def get(self, *args, **kwargs):
-        archives = Archive.select().where((Archive.status == 1) & (Archive.type == "archive")).order_by(Archive.published_time.desc())
+        archives = Archive.select().where((Archive.status == 1) & (Archive.type == "archive")).order_by(
+            Archive.published_time.desc())
         self.set_header("Content-Type", "application/xml")
         self.render("feed.xml", archives=archives)
